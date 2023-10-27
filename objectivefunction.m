d9da968f557c5dfcee00f4568db5f38270abfa89
aramesh2022 <113328896+aramesh2022@users.noreply.github.com>
2023-10-27 20:05:17 +1100

    Update objectivefunction.m  working depth sensing

--- objectivefunction.m
+++ objectivefunction.m
@@ -18,7 +18,7 @@ depthPC = rossubscriber('/camera/depth/points', 'sensor_msgs/PointCloud2');
 cmdVel = ros.Publisher(node, '/cmd_vel', 'geometry_msgs/Twist');
 odom = rossubscriber('/odom');
 
-while true
+
 % get images off cam
 rgbCAM = receive(rgb,5);
 rgbIM = readImage(rgbCAM);
@@ -34,17 +34,24 @@ PC_depth = receive(depthPC,5);
 camconvert = rgb2gray(rgbIM);
 
 points = detectHarrisFeatures(camconvert);
-corners = points.Location;
-imshow(rgbIM);
-hold on;
-plot(corners(:, 1), corners(:, 2), 'ro');
+% Sort the corner points by their strengths (you may use different criteria)
+[~, sorted_indices] = sort(points.Metric, 'descend');
 
-for i = 1:size(corners, 1)
-    for j = (i+1):size(corners, 1)
-        for k = (j+1):size(corners, 1)
-            for m = (k+1):size(corners, 1)
+% Select the strongest four corners
+strongest_corners = points.Location(sorted_indices(1:4), :);
+
+% Plot the original image
+imshow(camconvert);
+hold on;
+
+% Calculate the centers of the squares
+num_corners = size(strongest_corners, 1);
+for i = 1:num_corners
+    for j = (i+1):num_corners
+        for k = (j+1):num_corners
+            for m = (k+1):num_corners
                 % Calculate the center of the square as the average of the four corners
-                center = [mean(corners([i, j, k, m], 1)), mean(corners([i, j, k, m], 2))];
+                center = [mean(strongest_corners([i, j, k, m], 1)), mean(strongest_corners([i, j, k, m], 2))];
                 
                 % Plot green circles at the centers
                 plot(center(1), center(2), 'go');
@@ -52,8 +59,29 @@ for i = 1:size(corners, 1)
         end
     end
 end
+roundedC1 = round(center(1));
+roundedC2 = round(center(2));
+depth_value = depthIM(roundedC2, roundedC1)
+ odomMsg = receive(odom, 3);  % Wait for up to 3 seconds for odometry data
+    pose = odomMsg.Pose.Pose;
+    x_turtlebot = pose.Position.X;
+    y_turtlebot = pose.Position.Y;
+% Given depth reading (depth) and the camera center (cam_center)
+%depth = 1000;  % Depth reading in millimeters (adjust as needed)
+cam_center = [x_turtlebot, y_turtlebot];  % Camera center point in real-world coordinates
 
-   
+% Calculate the Euclidean distance
+square_center = [center(1), center(2)];  % Square's center point in real-world coordinates
+distance = norm(square_center - cam_center);
+
+% Display the calculated distance
+disp(['Distance to square: ', num2str(distance), ' millimeters']);   
+
+
+
+
+
+
     % Calculate control commands
     cmdVelMsg = rosmessage('geometry_msgs/Twist');
     cmdVelMsg.Linear.X = Kp_linear;  % Move forward/backward based on square's position
@@ -61,11 +89,11 @@ end
     
      send(cmdVel, cmdVelMsg);
      %
-     distance_to_square = sqrt(error_y^2);
- if distance_to_square <= max_tracking_distance
-        break;  % Exit the loop when the robot is within the specified distance
-    end
-end
+
+ % if distance_to_square <= max_tracking_distance
+ %        break;  % Exit the loop when the robot is within the specified distance
+ %    end
+ %    end
    % Publish control commands to drive topic
 
     % Check for the exit condition based on distance
